70b02c4d51077f47ecec2e34c61ee90e8882a220
amliyanage <ashenmliyanage@gmail.com>
2024-11-21 09:01:36 +0530

    add operators demonstration script

---
+++ oparaters.py
@@ -0,0 +1,140 @@
+# z = z + y
+# "=" is an assignment operator
+# "+" is an addition operator
+# "z" is a variable
+
+# division operator
+print(10/5)
+
+# modulas operator
+print(10%3)
+
+# floor division operator
+print(12//3)
+
+# exponent operator
+print(2**5)
+
+# multiplication operator
+print(2*5)
+
+# Assignment operator
+x = 5
+print(x)
+
+# Comparison operator
+print(5 == 5)
+print(5 != 5)
+print(5 > 5)
+print(5 < 5)
+print(5 >= 5)
+print(5 <= 5)
+
+# add and assign operator
+x += 5
+print(x)
+
+# subtract and assign operator
+x -= 5
+print(x)
+
+# multiply and assign operator
+x *= 5
+print(x)
+
+# divide and assign operator
+x /= 5
+print(x)
+
+# modulas and assign operator
+x %= 5
+print(x)
+
+# floor division and assign operator
+x //= 5
+print(x)
+
+# exponent and assign operator
+x **= 5
+print(x)    
+
+# comparison operator
+x = 5
+y = 10
+
+# equal operator
+print(x == y)
+
+# not equal operator
+print(x != y)
+
+# greater than operator
+print(x > y)
+
+# less than operator
+print(x < y)
+
+# greater than or equal to operator
+print(x >= y)
+
+# less than or equal to operator
+print(x <= y)
+
+
+#logical operator
+
+x = 5
+y = 10
+
+# and operator
+print(x > 3 and y < 20)
+
+# or operator
+print(x > 3 or y < 5)
+
+# not operator
+print(not(x > 3 and y < 20))
+
+# identity operator
+x = "halo"
+y = "halo"
+
+# is operator
+print(x is y)
+
+# is not operator
+print(x is not y)
+
+# membership operator
+
+x = ["apple","banana"]
+y = ["apple","banana"]
+
+# in operator
+print("banana" in x)
+
+# not in operator
+print("banana" not in y)
+
+# bitwise operator
+x = 10
+y = 4
+
+# bitwise and operator
+print(x & y)
+
+# bitwise or operator
+print(x | y)
+
+# bitwise xor operator
+print(x ^ y)
+
+# bitwise not operator
+print(~x)
+
+# bitwise left shift operator
+print(x << 2)
+
+# bitwise right shift operator
+print(x >> 2)
+
