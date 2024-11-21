5c8c7d47c2da31981b83633ba4895407ad290c4e
amliyanage <ashenmliyanage@gmail.com>
2024-11-21 08:57:51 +0530

    learn about if , elif and else

---
+++ controlFlow.py
@@ -0,0 +1,25 @@
+# conditional statements
+# if, elif, else
+
+# if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# if else statement
+if x < 15:
+    print("x is less than 5")
+else:
+    print("x is greater than 5")
+
+# if elif else statement
+if x < 6:
+    print("x is less than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is greater than 5")
+
+# ternary operator
+y = 5
+print("y is greater than 5") if y > 5 else print("y is less than 5")
