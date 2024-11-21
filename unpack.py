748265d4dc398ec0a1be25a018679904488cab79
amliyanage <ashenmliyanage@gmail.com>
2024-11-21 09:02:41 +0530

    learn about unpacking

---
+++ unpack.py
@@ -0,0 +1,27 @@
+my_list = [12,10,13]
+
+x , y , z = my_list
+
+print(x)
+print(y)
+print(z)
+
+# Output:
+# 12
+# 10
+# 13
+
+# typle packing
+my_tuple = (12,10,13)
+
+# typle unpacking
+x , y , z = my_tuple
+
+print(x)
+print(y)
+print(z)
+
+# Output:
+# 12
+# 10
+# 13
