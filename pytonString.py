39280f7a82e9e98633c4a910e1eaa7ef295932cf
amliyanage <ashenmliyanage@gmail.com>
2024-11-28 08:45:21 +0530

    study about string

---
+++ pytonString.py
@@ -0,0 +1,28 @@
+# # Multiline String
+
+# my_string  = """This is a multiline string with triple quotes 
+#                     you can use double quotes as well 
+#                     you can use single quotes as well
+#                     you can use triple quotes as well
+#                     you can use single quotes as well
+#                     """
+# print(my_string) # This is a multiline string with triple quotes
+
+# # lenth of the string
+# my_string = "Hello World"
+# print(len(my_string)) # 11
+
+# print(my_string[6]) # H
+
+# # slicing
+# print(my_string[1:5]) # ello
+
+# print(my_string.upper()) # HELLO WORLD
+# print(my_string.lower()) # hello world
+
+# print(my_string.replace("Hello", "Hi")) # Hi World
+
+my_string = "     well come to python programming     "
+print(my_string)
+my_string = my_string.strip()
+print(my_string)
